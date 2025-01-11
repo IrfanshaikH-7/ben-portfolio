1ff698f0b49d1305e345ca397b56a0d8022b5dc5
IrfanshaikH-7 <414irfann@gmail.com>
2025-01-11 22:29:07 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
 export default function Home() {
+    
   return (
-    <main className='w-full h-screen mt-[60px]'>
-      <div>
-        <h1 className='text-4xl italic'>Hello</h1>
-        <h2></h2>
+
+    <main className='h-full w-full'>
+          <section className='w-full h-screen  flex flex-col justify-center items-center'>
+      <div className='h-full w-full flex flex-col justify-center z-10 items-center '>
+        <h1 className='text-[10vw] font-serif italic text-orange-600  z-10 '>Benjamin</h1>
+        <div className='flex'>
+        <h2 className='  text-[15vw] font-extrabold  font-glock z-[9] -mt-36 leading-0'>Eisenstein</h2>
+        <h4 className='text-[2.5vw] font-extrabold font-glock'> Miller</h4>
+        </div>
       </div>
+      <section className=''>
+
+      </section>
+    </section>
+    <section className='w-full h-screen bg-black flex justify-center items-center'>
+
+
+    </section>
+
     </main>
+  
   );
 }
