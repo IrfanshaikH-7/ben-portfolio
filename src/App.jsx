72293c836240f267f603b410e7baee12c9db20e8
IrfanshaikH-7 <414irfann@gmail.com>
2025-01-11 21:30:35 +0530

    secondary

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/core/Header'
+import Home from './pages/Home'
+
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Home />
+  
+  </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className='w-full h-screen mt-[60px]'>
+      <div>
+        <h1 className='text-4xl italic'>Hello</h1>
+        <h2></h2>
+      </div>
+    </main>
+  );
+}
